072553c37130c1bc522eea849de8f86d952c51dc
Robert Gumeny <robert.gumeny528@gmail.com>
2020-04-05 16:43:06 -0600

    Weather box looks great

--- app/models/weather.js
+++ app/models/weather.js
@@ -5,7 +5,7 @@ export default class Weather {
     //      That is what this data returns! data.main.temp is the temperature in Kelvin
 
     this.city = data.name;
-    this.kelvin = data.main.temp;
+    this.kelvin = Math.round(data.main.temp);
     this.fahrenheit = Math.round(this.kelvin * (9 / 5) - 459.67);
     this.celsius = Math.floor(this.kelvin - 273.15);
     this.weather = data.weather[0].main;
@@ -14,11 +14,46 @@ export default class Weather {
 
   get Template() {
     return /*html*/ `
-    <p>Kelvin: ${this.kelvin}</p>
-    <p>Fahrenheit: ${this.fahrenheit}</p>
-    <p>Celsius: ${this.celsius}</p>
-    <p class="weather-city">${this.city}</p>
-      <p>${this.icon} , ${this.weather}</p>
+    <p class="mb-0">${this.city}</p>
+    <img src="http://openweathermap.org/img/wn/${this.icon}.png">
+    <p class="mt-0">${this.weather}</p>
+    <div id="weatherControls" class="carousel slide" date-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item text-center mt-2 active">
+        <p class="d-block">${this.fahrenheit}<span>&deg;</span>F</p>
+      </div>
+      <div class="carousel-item text-center mt-2">
+        <p class="d-block">${this.celsius}<span>&deg;</span>C</p>
+      </div>
+      <div class="carousel-item text-center mt-2">
+        <p class="d-block">${this.kelvin}<span>&deg;</span>K</p>
+      </div>
+    </div>
+    <a
+      class="carousel-control-prev"
+      href="#weatherControls"
+      role="button"
+      data-slide="prev"
+    >
+      <span
+        class="carousel-control-prev-icon"
+        aria-hidden="true"
+      ></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a
+      class="carousel-control-next"
+      href="#weatherControls"
+      role="button"
+      data-slide="next"
+    >
+      <span
+        class="carousel-control-next-icon"
+        aria-hidden="true"
+      ></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
     `;
   }
 }
